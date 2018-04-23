35da81f056e34518a5a48ae27293211e1d733972
tuvshinbt <tuvshin0bt@gmail.com>
2018-04-23 13:53:29 -0500

    Lab10

--- MockTransactionAPI/src/main/java/edu/mum/cs490/project/mock/transaction/api/util/MainTest.java
+++ MockTransactionAPI/src/main/java/edu/mum/cs490/project/mock/transaction/api/util/MainTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.mum.cs490.project.mock.transaction.api;
+package edu.mum.cs490.project.mock.transaction.api.util;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
